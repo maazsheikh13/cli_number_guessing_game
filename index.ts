28536fceeaa418fd929ffd50237d87e38a7cdcbc
maazsheikh13 <165181534+maazsheikh13@users.noreply.github.com>
2024-04-08 20:35:14 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\twellcome to codewithMaaz - CLI number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() *5+1);
+
+const answer= await inquirer.prompt([
+    {
+        name:"userGuestNumber",
+        type:"number",
+        message:"Enter your guest number(number limits from 1 to 5):",
+
+    },
+]
+);
+if(answer.userGuestNumber === randomNumber){
+console.log("congratulations ! you got the correct guest number.");
+} 
+else{
+    console.log("sorry! you got the wrong guest number.");
+}
